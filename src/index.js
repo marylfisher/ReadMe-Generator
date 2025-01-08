792dd9f1b791047ceece44e2c72bb8184866b070
marylfisher <marylaurenfisher@gmail.com>
2025-01-08 10:39:25 -0700

    added prompt code

--- src/index.js
+++ src/index.js
@@ -5,6 +5,37 @@ const generateReadMe = ({}) => {}
 
 inquirer
     .prompt([
+        {
+            type: 'input',
+            message: 'Enter a dscription of your project.',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Enter the installation instructions.',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Enter the usage information.',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Enter your contribution guidelines.',
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: 'Enter your test instructions.',
+            name: 'test',
+        },
+        {
+            type: 'list',
+            message: 'Which license is your project using?',
+            name:'license',
+            choices: ['MIT License', 'Apache License 2.0', 'GNU General Public License (GPL)', 'BSD Licenses', 'Creative Commons Attribution-NonCommercial-NoDerivs (CC-BY-NC-ND)', 'Unlicense']
+        }
 
     ])
     .then ()
